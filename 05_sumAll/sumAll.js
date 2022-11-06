5403a53b90c11bb0827a66bb4499a7fac7599978
as00r <as00r.dev@gmail.com>
2022-11-06 21:57:11 +0530

    Finish exercise 5
    Use Gauss method to find sum.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	if (
+		typeof start != "number" ||
+		typeof end != "number" ||
+		start < 0 ||
+		end < 0
+	) {
+		return "ERROR";
+	}
 
+	if (start > end) {
+		[start, end] = [end, start];
+	}
+
+	const sumFromOneToEnd = (end * (end + 1)) / 2;
+	const sumFromOneToStart = (start * (start + 1)) / 2;
+
+	return sumFromOneToEnd - sumFromOneToStart + 1;
 };
 
 // Do not edit below this line
