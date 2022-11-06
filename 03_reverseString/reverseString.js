90a6d5dda728d1bfc25eeddfb06ca865d6dba4fd
as00r <as00r.dev@gmail.com>
2022-11-06 19:02:12 +0530

    Finish Exercise 3
    Create a new empty string and traverse the argument
    string from the end to start and concatenate the letter to the new
    string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (stringToBeReversed) {
+	let reversedString = "";
+	for (let i = stringToBeReversed.length - 1; i > -1; i--) {
+		reversedString = `${reversedString}${stringToBeReversed.charAt(i)}`;
+	}
+	return reversedString;
 };
 
 // Do not edit below this line
