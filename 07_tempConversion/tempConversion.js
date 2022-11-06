287f51a6bf0a059180e8ae3cbd453b296b8623c9
as00r <as00r.dev@gmail.com>
2022-11-06 23:05:23 +0530

    Finish exercise 7
    To round number to n decimal place, use number.toFixed(n). This
    returns a string so +number.toFixed(n) returns a number.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (tempInF) {
+	const tempInC = (tempInF - 32) * (5 / 9);
+	return +tempInC.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (tempInC) {
+	const tempInF = tempInC * (9 / 5) + 32;
+	return +tempInF.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
